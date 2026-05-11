2d889011fa1e976679b4d6ecf9910545f3911299
thuan0222 <tranhuuthuan05021996@icloud.com>
2026-05-11 18:10:19 +0700

    DFAXCXC

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/TiemhoaPhuclinh',
-  assetPrefix: '/TiemhoaPhuclinh',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
