f5c414e2fd9912fcb160c93887b3c18278967ba8
thuan0222 <tranhuuthuan05021996@icloud.com>
2026-05-11 17:57:41 +0700

    ADD FILE

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
+  basePath: '/TiemhoaPhuclinh',
+  assetPrefix: '/TiemhoaPhuclinh',
   images: {
     unoptimized: true,
-
     remotePatterns: [
       {
         protocol: 'https',
@@ -17,5 +17,4 @@ const nextConfig = {
     ],
   },
 };
-
 export default nextConfig;
